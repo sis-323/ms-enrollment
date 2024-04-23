a4906b83db34c9dae550c5d1c03b0d99f831beb3
Sergio Guachalla <sergioguachalla0@gmail.com>
2024-04-22 22:04:35 -0400

    update dockerfile

---
+++ src/main/kotlin/com/enrollment/msenrollment/bl/DeliverableBl.kt
@@ -0,0 +1,4 @@
+package com.enrollment.msenrollment.bl
+
+class DeliverableBl {
+}
---
+++ src/main/kotlin/com/enrollment/msenrollment/dto/DeliverableDto.kt
@@ -0,0 +1,8 @@
+package com.enrollment.msenrollment.dto
+
+data class DeliverableDto(
+    val title: String,
+    val dueDate: String,
+    val description: String,
+
+)
